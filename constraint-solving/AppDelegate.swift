1cfa348635da832720e54251e0e28bb617e09a0f
Alexey <alexey.kozov@gmail.com>
2017-01-17 23:33:06 +0300

    Initial Commit

---
+++ constraint-solving/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  constraint-solving
+//
+//  Created by vvoZokk on 17.01.17.
+//  Copyright © 2017 BMSTU. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
